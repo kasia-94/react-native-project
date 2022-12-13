35b3e73e38db0c841b80f9035315c842f78d97c9
Kateryna Berehovych <kasia-94@ukr.net>
2022-12-13 18:45:59 +0200

    Add src

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import LoginScreen from "./Screens/LoginScreen";
-import RegistrationScreen from "./Screens/RegistrationScreen";
+import React, { useState, useRoute } from "react";
+import LoginScreen from "./src/Screens/LoginScreen";
+import RegistrationScreen from "./src/Screens/RegistrationScreen";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
+  // const routing = useRoute(null);
 
   const loadFonts = async () => {
     await Font.loadAsync({
-      "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-      "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-      "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+      "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+      "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+      "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
     });
   };
 
